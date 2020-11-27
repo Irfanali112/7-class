9c4d8dae5926a90c2b1e177e0492625150493858
unknown <you@example.com>
2020-11-27 17:55:57 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// let num = ["sda", "sad" ,"sad", 7 , 234234234 , 3 , 45];
+
+
+
+
+// console.log(num);
+
+
+let items =["roze", "lili" ,"jasmin","sunflower" ,"kanwal"];
+
+
+// items.splice(2,2);
+
+// console.log("after operation:", items);
+
+// /////////////////////////////////////////////////////////////////////////////////////////////
+// let items =["roze", "lili" ,"jasmin","sunflower" ,"kanwal"];
+
+
+// let newitems=items.slice(-5,-3);
+
+// console.log("new items:", newitems);
+
+////////////////////////////////////////////for loop
+
+for(let i =2; i <= 20 ; i=i+2){
+
+       console.log( i )
+
+}
